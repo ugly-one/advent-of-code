e0b6210f7bbff4001027cb4fb936f7486e6e2beb
UglyOne <omojzboze@protonmail.com>
2022-12-10 20:16:22 +0100

    Program - execute a method that actually exists

--- Program.fs
+++ Program.fs
@@ -4,6 +4,4 @@ open advent_of_code_2022.Day10
 let readLines file = System.IO.File.ReadLines(file)
 let input = readLines Day10.inputFile |> Array.ofSeq
 
-// Day10.run input |> printfn "%d"
-
-Day10.test input
+Day10.run_part2 input
