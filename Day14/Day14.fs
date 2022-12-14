46f821845935c6d10d3d8e27e15549367eb0ae16
UglyOne <omojzboze@protonmail.com>
2022-12-14 11:01:50 +0100

    Day14 - created map

--- Day14/Day14.fs
+++ Day14/Day14.fs
@@ -11,7 +11,28 @@ type Path = {
     Positions: Position array
 }
 
-let part1 (input: string array) = 
+type Cell = 
+    | Rock
+    | Air
+
+type Map = {
+    Cells: Cell array
+    Width: int
+    Height: int
+}
+
+let print (map: Map) = 
+    for y in 0..map.Height-1 do 
+        for x in 0..map.Width-1 do 
+            match map.Cells[x + y * map.Height] with 
+            | Rock -> printf "#"
+            | Air -> printf "."
+        printfn ""
+
+let part1 mapSize (input: string array)  = 
+    let width = mapSize
+    let height = mapSize
+    let map = Array.create (width * height) Air
 
     let paths = Array.zeroCreate input.Length
     let mutable pathIndex = 0;
@@ -23,16 +44,32 @@ let part1 (input: string array) =
         for _match in matches do 
             let x = _match.Groups[1].Value |> int
             let y = _match.Groups[2].Value |> int
-            let pos = {X = x; Y = y}
+            let pos = {X = x - 480; Y = y}
             path[index] <- pos
+            map[pos.X + height*pos.Y] <- Cell.Rock
+            if index - 1 >= 0 
+            then 
+                let previousPosition = path[index-1]
+                let diffX = pos.X - previousPosition.X
+                let diffY = pos.Y - previousPosition.Y
+                if (diffX <> 0) 
+                then 
+                    for a in 1..abs diffX do 
+                        if diffX > 0 then map[pos.X + a + height*pos.Y] <- Cell.Rock else map[pos.X + a + height*pos.Y] <- Cell.Rock
+                else 
+                     for a in 1..abs diffY do 
+                        if diffY > 0 then map[pos.X + height*(pos.Y-a)] <- Cell.Rock else map[pos.X + height*(pos.Y+a)] <- Cell.Rock
+            else 
+                ()
             index <- index + 1
             ()
         paths[pathIndex] <- path
         pathIndex <- pathIndex + 1
 
+    print {Cells = map; Width = width; Height = height}
     let a = paths.Length
     0
 
 let run () = 
-    Day13.testcase "Day14/testInput.txt" 24 part1
-    Day13.testcase "Day14/input.txt" 24 part1
+    Day13.testcase "Day14/testInput.txt" 24 (part1 30)
+    Day13.testcase "Day14/input.txt" 24 (part1 200)
