f4f9316e90e2e93e8d2af45daf382283ef70d4fb
UglyOne <omojzboze@protonmail.com>
2022-12-14 22:07:40 +0100

    Day14 part1 done

--- Day14/Day14.fs
+++ Day14/Day14.fs
@@ -2,6 +2,7 @@
 
 open System.Text.RegularExpressions
 open System.Collections.Generic
+open System
 
 type Position = {
     X: int
@@ -26,11 +27,12 @@ type Map = {
 let print (map: Map) = 
     for y in 0..map.Height-1 do 
         for x in 0..map.Width-1 do 
-            match map.Cells[x + y * map.Height] with 
+            match map.Cells[x + y * map.Width] with 
             | Rock ->   printf "#"
             | Air ->    printf "."
             | Sand ->   printf "o"
         printfn ""
+    printfn ""
 
 let createPaths (input: string array) = 
     let paths = new List<Path>()
@@ -51,7 +53,35 @@ let createPaths (input: string array) =
             if (x > maxX) then maxX <- x else ()
             if (y > maxY) then maxY <- y else ()
         paths.Add({Positions = path})
-    paths, minX, maxX, maxY
+    paths.ToArray (), minX, maxX, maxY
+
+let createMap2 (input: string array) floor = 
+    let (paths, minX, maxX, maxY) = createPaths input
+    let height = maxY + 1 + floor
+    let width = maxX - minX + 1
+    let map = Array.create (width * height) Air
+    for path in paths do
+        for positionIndex in 0..(path.Positions.Length - 1) do
+            let pos = path.Positions[positionIndex]
+            let pos = {X = pos.X - minX; Y = pos.Y}
+            map[pos.X + width*pos.Y] <- Cell.Rock
+            if positionIndex - 1 >= 0 
+            then 
+                let previousPosition = path.Positions[positionIndex-1]
+                let previousPosition =  {X = previousPosition.X - minX; Y = previousPosition.Y}
+                let diffX = pos.X - previousPosition.X
+                let diffY = pos.Y - previousPosition.Y
+                if (diffX <> 0) 
+                then 
+                    for a in 1..abs diffX do 
+                        if diffX > 0 then map[pos.X - a + width*pos.Y] <- Cell.Rock else map[pos.X + a + width*pos.Y] <- Cell.Rock
+                else 
+                     for a in 1..abs diffY do 
+                        if diffY > 0 then map[pos.X + width*(pos.Y-a)] <- Cell.Rock else map[pos.X + width*(pos.Y+a)] <- Cell.Rock
+            else 
+                ()
+    {Cells = map; Height = height; Width = width}, minX
+
 
 let createMap mapSize (input: string array) = 
     let width = mapSize
@@ -89,41 +119,60 @@ let createMap mapSize (input: string array) =
 let isEmpty cell = 
     if cell = Air then true else false
 
+let isWithinMap map position = 
+    if position.X < 0 || position.Y < 0 then false else 
+        if position.X >= map.Width || position.Y >= map.Height then false else true
+
 let rec dropSand position map = 
+
     let positionBelow = {X = position.X; Y = position.Y + 1}
-    let cellBelow = map.Cells[positionBelow.X + (positionBelow.Y) * map.Height]
-    if isEmpty cellBelow
-    then dropSand positionBelow map
-    else 
-        let positionBelowLeft = {X = position.X - 1; Y = position.Y + 1}
-        let cellBelowLeft = map.Cells[positionBelowLeft.X + positionBelowLeft.Y * map.Height]
-        if isEmpty cellBelowLeft 
-        then dropSand positionBelowLeft map 
+    if not (isWithinMap map positionBelow) then false else
+        let cellBelow = map.Cells[positionBelow.X + (positionBelow.Y) * map.Width]
+        if isEmpty cellBelow
+        then dropSand positionBelow map
         else 
-            let positionBelowRight = {X = position.X + 1; Y = position.Y + 1}
-            let cellBelowLeft = map.Cells[positionBelowRight.X + positionBelowRight.Y * map.Height]
-            if isEmpty cellBelowLeft 
-            then dropSand positionBelowRight map 
-            else map.Cells[position.X + position.Y * map.Height] <- Sand
+            let positionBelowLeft = {X = position.X - 1; Y = position.Y + 1}
+            if not (isWithinMap map positionBelowLeft) then false else
+                let cellBelowLeft = map.Cells[positionBelowLeft.X + positionBelowLeft.Y * map.Width]
+                if isEmpty cellBelowLeft 
+                then dropSand positionBelowLeft map 
+                else 
+                    let positionBelowRight = {X = position.X + 1; Y = position.Y + 1}
+                    if not (isWithinMap map positionBelowRight) then false else
+                        let cellBelowRight = map.Cells[positionBelowRight.X + positionBelowRight.Y * map.Width]
+                        if isEmpty cellBelowRight 
+                        then dropSand positionBelowRight map 
+                        else 
+                            if isWithinMap map position then  
+                                map.Cells[position.X + position.Y * map.Width] <- Sand
+                                true
+                            else false
 
 
-let part1 mapSize (input: string array)  = 
-    let map = createMap mapSize input
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    dropSand {X = 20; Y = 0} map
-    print map
-    0
+let part1 (input: string array)  = 
+    let (map, minX) = createMap2 input 0
+    let mutable counter = 0
+    let mutable drop = true
+    while drop do 
+        //print map
+        drop <- dropSand {X = 500-minX; Y = 0} map
+        if drop then counter <- counter + 1 else ()
+       // Console.Clear ()
+    counter
+
+let part2 (input: string array)  = 
+    let (map, minX) = createMap2 input 2
+    let mutable counter = 0
+    let mutable drop = true
+    while drop do 
+        print map
+        drop <- dropSand {X = 500-minX; Y = 0} map
+        if drop then counter <- counter + 1 else ()
+       // Console.Clear ()
+    counter
 
 let run () = 
-    Day13.testcase "Day14/testInput.txt" 24 (part1 30)
-    //Day13.testcase "Day14/input.txt" 24 (part1 200)
+    //Day13.testcase "Day14/testInput.txt" 24 part1
+    //Day13.testcase "Day14/input.txt" 737 part1
+
+    Day13.testcase "Day14/testInput.txt" 93 part2
