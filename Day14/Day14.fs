e91657a92cdac8618149c096cbc7477f29b3c08b
UglyOne <omojzboze@protonmail.com>
2022-12-14 10:05:31 +0100

    Day14 init

---
+++ Day14/Day14.fs
@@ -0,0 +1,38 @@
+﻿module Day14
+
+open System.Text.RegularExpressions
+
+type Position = {
+    X: int
+    Y: int
+}
+
+type Path = {
+    Positions: Position array
+}
+
+let part1 (input: string array) = 
+
+    let paths = Array.zeroCreate input.Length
+    let mutable pathIndex = 0;
+
+    for line in input do
+        let matches = Regex.Matches(line, "([0-9]+),([0-9]+)")
+        let path = Array.zeroCreate matches.Count
+        let mutable index = 0
+        for _match in matches do 
+            let x = _match.Groups[1].Value |> int
+            let y = _match.Groups[2].Value |> int
+            let pos = {X = x; Y = y}
+            path[index] <- pos
+            index <- index + 1
+            ()
+        paths[pathIndex] <- path
+        pathIndex <- pathIndex + 1
+
+    let a = paths.Length
+    0
+
+let run () = 
+    Day13.testcase "Day14/testInput.txt" 24 part1
+    Day13.testcase "Day14/input.txt" 24 part1
