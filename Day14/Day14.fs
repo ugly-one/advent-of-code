975e4c4a59432a8b2e0dd04946837cc8ff8e3bd4
UglyOne <omojzboze@protonmail.com>
2022-12-14 12:24:49 +0100

    trying to get the size of the map

--- Day14/Day14.fs
+++ Day14/Day14.fs
@@ -1,6 +1,7 @@
 ﻿module Day14
 
 open System.Text.RegularExpressions
+open System.Collections.Generic
 
 type Position = {
     X: int
@@ -14,6 +15,7 @@ type Path = {
 type Cell = 
     | Rock
     | Air
+    | Sand
 
 type Map = {
     Cells: Cell array
@@ -25,20 +27,39 @@ let print (map: Map) =
     for y in 0..map.Height-1 do 
         for x in 0..map.Width-1 do 
             match map.Cells[x + y * map.Height] with 
-            | Rock -> printf "#"
-            | Air -> printf "."
+            | Rock ->   printf "#"
+            | Air ->    printf "."
+            | Sand ->   printf "o"
         printfn ""
 
-let part1 mapSize (input: string array)  = 
+let createPaths (input: string array) = 
+    let paths = new List<Path>()
+    let mutable minX = 9999
+    let mutable maxX = 0
+    let mutable maxY = 0
+    for line in input do
+        let matches = Regex.Matches(line, "([0-9]+),([0-9]+)")
+        let path = Array.zeroCreate matches.Count
+        let mutable index = 0
+        for _match in matches do 
+            let x = _match.Groups[1].Value |> int
+            let y = _match.Groups[2].Value |> int
+            let pos = {X = x; Y = y}
+            path[index] <- pos
+            index <- index + 1
+            if (x < minX) then minX <- x else ()
+            if (x > maxX) then maxX <- x else ()
+            if (y > maxY) then maxY <- y else ()
+        paths.Add({Positions = path})
+    paths, minX, maxX, maxY
+
+let createMap mapSize (input: string array) = 
     let width = mapSize
     let height = mapSize
     let map = Array.create (width * height) Air
-
-    let paths = Array.zeroCreate input.Length
-    let mutable pathIndex = 0;
-
     for line in input do
         let matches = Regex.Matches(line, "([0-9]+),([0-9]+)")
+        // TODO no need to have an array of paths, we need only the previous one
         let path = Array.zeroCreate matches.Count
         let mutable index = 0
         for _match in matches do 
@@ -63,13 +84,46 @@ let part1 mapSize (input: string array)  =
                 ()
             index <- index + 1
             ()
-        paths[pathIndex] <- path
-        pathIndex <- pathIndex + 1
+    {Cells = map; Width = width; Height = height}
 
-    print {Cells = map; Width = width; Height = height}
-    let a = paths.Length
+let isEmpty cell = 
+    if cell = Air then true else false
+
+let rec dropSand position map = 
+    let positionBelow = {X = position.X; Y = position.Y + 1}
+    let cellBelow = map.Cells[positionBelow.X + (positionBelow.Y) * map.Height]
+    if isEmpty cellBelow
+    then dropSand positionBelow map
+    else 
+        let positionBelowLeft = {X = position.X - 1; Y = position.Y + 1}
+        let cellBelowLeft = map.Cells[positionBelowLeft.X + positionBelowLeft.Y * map.Height]
+        if isEmpty cellBelowLeft 
+        then dropSand positionBelowLeft map 
+        else 
+            let positionBelowRight = {X = position.X + 1; Y = position.Y + 1}
+            let cellBelowLeft = map.Cells[positionBelowRight.X + positionBelowRight.Y * map.Height]
+            if isEmpty cellBelowLeft 
+            then dropSand positionBelowRight map 
+            else map.Cells[position.X + position.Y * map.Height] <- Sand
+
+
+let part1 mapSize (input: string array)  = 
+    let map = createMap mapSize input
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
+    dropSand {X = 20; Y = 0} map
+    print map
     0
 
 let run () = 
     Day13.testcase "Day14/testInput.txt" 24 (part1 30)
-    Day13.testcase "Day14/input.txt" 24 (part1 200)
+    //Day13.testcase "Day14/input.txt" 24 (part1 200)
