68bbf262a07175a56e26ecad3ca03d6c064ad67a
UglyOne <omojzboze@protonmail.com>
2023-01-02 11:31:53 +0100

    Day 17 part 1. test input works but not the real input.

--- Program.fs
+++ Program.fs
@@ -1,11 +1,1 @@
-﻿Day16_attempt2.part1TestInput ()
-
-Day16_attempt2.part1RealInput ()
-
-Performance.measure (fun () -> Day16_attempt2.part1RealInput ()) "part 1"
-
-Day16_attempt2.part2TestInput ()
-
-Day16_attempt2.part2RealInput ()
-
-Performance.measure (fun () -> Day16_attempt2.part2RealInput ()) "part 2"
+﻿Day17.run_part1 ()
