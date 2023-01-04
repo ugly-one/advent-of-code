b0e3c2f76884a866d8cae8610463a1e7d39ce45d
UglyOne <omojzboze@protonmail.com>
2023-01-04 15:13:04 +0100

    Day 17 part 2 done

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿//Day17.run_part1 ()
 
-Performance.measure (fun () -> Day17.run_part1_test ()) "part1 test"
-Performance.measure (fun () -> Day17.run_part1_input ()) "part1 real input"
+//Performance.measure (fun () -> Day17.run_part1_test ()) "part1 test"
+//Performance.measure (fun () -> Day17.run_part1_input ()) "part1 real input"
 
-Performance.measure (fun () -> Day17.run_part2_test ()) "part2 test"
+//Performance.measure (fun () -> Day17.run_part2_test ()) "part2 test"
+Performance.measure (fun () -> Day17.run_part2_input ()) "part2 real input"
