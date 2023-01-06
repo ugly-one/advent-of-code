0547c755f806dceabecfa988258f51c3cc15c019
UglyOne <omojzboze@protonmail.com>
2023-01-06 13:18:27 +0100

    Day 18 part 1 done

---
+++ Day18/Day18.fs
@@ -0,0 +1,69 @@
+﻿module Day18
+
+let getSurfaceArea (cubeX,cubeY,cubeZ) =
+    [|
+        (cubeX, cubeY, cubeZ + 1);
+        (cubeX, cubeY, cubeZ - 1);
+        (cubeX, cubeY + 1, cubeZ);
+        (cubeX, cubeY - 1, cubeZ);
+        (cubeX + 1, cubeY, cubeZ);
+        (cubeX - 1, cubeY, cubeZ);
+    |]
+
+let addCube totalSurfaceArea allPoints newPoint = 
+    let newPointSurfaceArea = getSurfaceArea newPoint |> Array.ofSeq
+
+    if totalSurfaceArea |> Seq.contains newPoint then 
+        // step 1, remove all points from totalSurfaceArea equal to point
+        let newTotalSurfaceArea = totalSurfaceArea |> Array.filter (fun areaPoint -> newPoint <> areaPoint)
+        // step 2, remove all points from cubeSurfaceArea that are found in allPoints
+        let newPointSurfaceAreaWithoutCollisions = newPointSurfaceArea |> Array.filter (fun pointFromNewPointSurface -> Array.contains pointFromNewPointSurface allPoints |> not)
+        (Array.append newTotalSurfaceArea newPointSurfaceAreaWithoutCollisions, Array.append allPoints [| newPoint |])
+    else 
+        (Array.append totalSurfaceArea newPointSurfaceArea, Array.append allPoints [| newPoint |])
+
+
+let run input = 
+    input |> Seq.fold (fun (surfaceArea, allPoints) cube -> addCube surfaceArea allPoints cube) (Array.empty, Array.empty)
+
+
+let runTestCases () = 
+    let cubes = 
+        [|
+            (1,1,1);
+            (2,1,1)
+        |]
+    let result = run cubes |> fst
+    if result.Length <> 10 then printfn $"Test case 1 Wrong answer {result.Length}" else printfn "Test case 1 OK"
+
+    let cubes = 
+        [|
+            (1,1,1);
+            (3,1,1);
+            (2,1,1)
+        |]
+    let result = run cubes |> fst
+    if result.Length <> 14 then printfn $"Test case 2 Wrong answer {result.Length}" else printfn "Test case 2 OK"
+
+    let cubes = 
+        [|
+            (2,2,2);
+            (1,2,2);
+            (3,2,2);
+            (2,1,2);
+            (2,3,2);
+            (2,2,1);
+            (2,2,3);
+            (2,2,4);
+            (2,2,6);
+            (1,2,5);
+            (3,2,5);
+            (2,1,5);
+            (2,3,5);
+        |]
+    let result = run cubes |> fst
+    if result.Length <> 64 then printfn $"Test case 3 Wrong answer {result.Length}" else printfn "Test case 3 OK"
+
+    let cubes = Day18_input.input
+    let result = run cubes |> fst
+    if result.Length <> 3550 then printfn $"Test case 4 Wrong answer {result.Length}" else printfn "Test case 4 OK"
--- Program.fs
+++ Program.fs
@@ -1,7 +1,1 @@
-﻿//Day17.run_part1 ()
-
-//Performance.measure (fun () -> Day17.run_part1_test ()) "part1 test"
-//Performance.measure (fun () -> Day17.run_part1_input ()) "part1 real input"
-
-//Performance.measure (fun () -> Day17.run_part2_test ()) "part2 test"
-Performance.measure (fun () -> Day17.run_part2_input ()) "part2 real input"
+﻿Day18.runTestCases()
