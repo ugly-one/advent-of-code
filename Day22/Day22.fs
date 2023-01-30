eb2b5fcbb16b4169dd831784eed4fe0259a0528c
UglyOne <omojzboze@protonmail.com>
2023-01-30 09:51:38 +0100

    Day22 part 1 done

--- Day22/Day22.fs
+++ Day22/Day22.fs
@@ -15,7 +15,6 @@ type Direction =
 type Map = 
     {
         Content: string[]
-        Width: int
         Height: int
     }
 
@@ -26,44 +25,40 @@ let getOppositeDirection (direction: Direction) =
     | Left -> Right
     | Right -> Left
 
+let move (pos: Position) (direction: Direction) (map: Map) = 
+    let (x, y) = 
+        match direction with 
+        | Up -> if pos.Y = 0 then (pos.X, map.Height - 1) else (pos.X, pos.Y - 1)
+        | Down -> if pos.Y = map.Height - 1 then (pos.X, 0) else (pos.X, pos.Y + 1)
+        | Left -> if pos.X = 0 then (map.Content[pos.Y].Length - 1, pos.Y) else (pos.X - 1, pos.Y)
+        | Right -> if pos.X = map.Content[pos.Y].Length - 1 then (0, pos.Y) else (pos.X + 1, pos.Y)
+    {X = x; Y = y}    
+
 let moveUntilMapsEnd (pos: Position) (direction: Direction) (map: Map) = 
-    // assume we cannot go outside of map's range
     let mutable keepGoing = true
     let mutable result = pos
     let mutable newPos = pos
     while keepGoing do 
-        newPos <- 
-            match direction with 
-            | Up -> {X = newPos.X; Y = newPos.Y - 1}
-            | Down -> {X = newPos.X; Y = newPos.Y + 1}
-            | Left -> {X = newPos.X - 1; Y = newPos.Y}
-            | Right -> {X = newPos.X + 1; Y = newPos.Y}
+        newPos <- move newPos direction map
         if map.Content[newPos.Y][newPos.X] = ' ' then keepGoing <- false else result <- newPos
     result
 
-let move (pos: Position) (direction: Direction) (steps: int) (map: Map) = 
-    let mutable (endPositionX, endPositionY) = (pos.X, pos.Y)
+let walk (pos: Position) (direction: Direction) (steps: int) (map: Map) = 
+    let mutable endPosition = pos
     for step in 1 .. 1 .. steps do 
-        let (newPositionX, newPositionY) = 
-            match direction with 
-            | Up -> if endPositionY = 0 then (endPositionX, map.Height - 1) else (endPositionX, endPositionY - 1)
-            | Down -> if endPositionY = map.Height - 1 then (endPositionX, 0) else (endPositionX, endPositionY + 1)
-            | Left -> if endPositionX = 0 then (map.Content[endPositionY].Length - 1, endPositionY) else (endPositionX - 1, endPositionY)
-            | Right -> if endPositionX = map.Content[endPositionY].Length - 1 then (0, endPositionY) else (endPositionX + 1, endPositionY)
-        if map.Content[newPositionY][newPositionX] = '.' 
+        let newPosition = move endPosition direction map
+        if map.Content[newPosition.Y][newPosition.X] = '.' 
             then 
-                endPositionX <- newPositionX
-                endPositionY <- newPositionY
-        elif map.Content[newPositionY][newPositionX] = '#'
+                endPosition <- newPosition
+        elif map.Content[newPosition.Y][newPosition.X] = '#'
             then () // TODO stop the loop!
         else 
-            let positionAfterWrappingAround = moveUntilMapsEnd {X = newPositionX; Y = newPositionY} (getOppositeDirection direction) map
+            let positionAfterWrappingAround = moveUntilMapsEnd newPosition (getOppositeDirection direction) map
             if map.Content[positionAfterWrappingAround.Y].[positionAfterWrappingAround.X] = '#' 
                 then () // TODO stop the loop
                 else
-                    endPositionX <- positionAfterWrappingAround.X
-                    endPositionY <- positionAfterWrappingAround.Y
-    { X = endPositionX; Y = endPositionY}
+                    endPosition <- positionAfterWrappingAround
+    endPosition
 
 type Rotation = 
     | CounterClockWise
@@ -105,9 +100,18 @@ let directionValue direction =
 
 let parse (input: string[]) = 
     let map = input[0..input.Length - 3]
+    let mapWidth = map |> Array.fold (fun width row -> if row.Length > width then row.Length else width) 0
+    let extend (row: string) = 
+        if row.Length < mapWidth then 
+            let missingPartLength = mapWidth - row.Length
+            let missingPart = String.init missingPartLength (fun i -> " ")
+            row + missingPart
+        else 
+            row
+    let map = map |> Array.map (fun row -> extend row)
     let startPosition = Seq.findIndex (fun char -> char = '.') map[0]
     let pathToTake = input[input.Length - 1] |> parsePath
-    ({Content = map; Width = 15; Height = map.Length}, startPosition, pathToTake)
+    ({Content = map; Height = map.Length}, startPosition, pathToTake)
 
 let parseOnTestInput () = 
     let input = inputReader.readLines "Day22/testInput.txt"
@@ -119,7 +123,7 @@ let runPart1 input =
     let mutable direction = Right
     for movement in path do 
         match movement with 
-        | Walk steps -> position <- move position direction steps map
+        | Walk steps -> position <- walk position direction steps map
         | Rotate rotation -> direction <- rotate rotation direction
     (position, direction)
 
@@ -127,29 +131,37 @@ let testGoingDown () =
     let (map, startPosition, _) = parseOnTestInput ()
     let direction = Down
     let steps = 1
-    let position = move {X = startPosition; Y = 0} direction steps map
+    let position = walk {X = startPosition; Y = 0} direction steps map
     if position <> {X = 8; Y = 1} then failwith "you cannot go even one step down" else printfn "Going down one step OK"
 
 let testHittingWall () = 
     let (map, startPosition, _) = parseOnTestInput ()
     let direction = Right
     let steps = 678
-    let position = move {X = startPosition; Y = 0}  direction steps map
+    let position = walk {X = startPosition; Y = 0}  direction steps map
     if position <> {X = 10; Y = 0} then failwith "did you miss the wall??" else printfn "Stopping on a wall OK"
 
 let testWrappingAroundVertical () = 
     let (map, _, _) = parseOnTestInput ()
     let direction = Up
     let steps = 1
-    let position = move {X = 5; Y = 4} direction steps map
+    let position = walk {X = 5; Y = 4} direction steps map
     if position <> {X = 5; Y = 7} then failwith "did you just fall from the cliff?? (simple UP)" else printfn "Wrapping around (simple UP) OK"
 
 let testWrappingAroundHorizontal() = 
     let (map, _, _) = parseOnTestInput ()
     let direction = Left
     let steps = 100
-    let position = move {X = 5; Y = 7} direction steps map
+    let position = walk {X = 5; Y = 7} direction steps map
     if position <> {X = 11; Y = 7} then failwith "did you just fall from the cliff?? (simple LEFT)" else printfn "Wrapping around (simple LEFT) OK"
+
+let testWrappingAroundVertical_2 () = 
+    let (map, startingPosition, _) = inputReader.readLines "Day22/testInput2.txt" |> parse 
+    let direction = Up
+    let steps = 1
+    let position = walk {X = startingPosition; Y = 0} direction steps map
+    if position <> {X = startingPosition; Y = 7} then failwith "did you just fall from the cliff?? (complex UP)" else printfn "Wrapping around (complex UP) OK"
+
 
 let testInput () = 
     let input = inputReader.readLines "Day22/testInput.txt"
@@ -163,7 +175,7 @@ let testReal () =
     let input = inputReader.readLines "Day22/input.txt"
     let (position, direction) = runPart1 input
     let result = 1000 * (position.Y + 1) + 4 * (position.X + 1) + (directionValue direction)
-    if result <> 6032 then failwith "failed test input - final calculation" else printfn "Test input OK"
+    if result <> 6032 then failwithf "failed real input - final calculation. %d" result else printfn "real input OK"
     ()
 
 let part1() = 
@@ -171,6 +183,7 @@ let part1() =
     testHittingWall ()
     testWrappingAroundVertical ()
     testWrappingAroundHorizontal ()
+    testWrappingAroundVertical_2 ()
     testInput ()
     testReal ()
     ()
