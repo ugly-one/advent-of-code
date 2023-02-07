be8861105661903d7e49c65679c5907cea69cdc0
UglyOne <omojzboze@protonmail.com>
2023-02-07 22:07:47 +0100

    day 24 part 1, work in progress

--- Day24/Day24.fs
+++ Day24/Day24.fs
@@ -31,6 +31,15 @@ let part1 () =
     let width = input[0].Length
     let height = input.Length
 
+    let blizzards = seq {
+        for y in 0 .. 1 .. input.Length - 1 do 
+            for x in 0 .. 1 .. input[0].Length - 1 do 
+                match input[y][x] with 
+                | '.' -> ()
+                | '#' -> ()
+                | a -> yield (x, y)
+    }
+
     let map : Cell[][] = Array.create width (Array.create height Wall)
 
     let charToBlizzard char = 
@@ -46,4 +55,62 @@ let part1 () =
         map[y] <- row
 
     print map
+
+    let getPositionToUpdate blizzard x y = 
+        match blizzard with 
+            | '>' -> 
+                match map[y][x + 1] with 
+                | Field f -> (x + 1, y, f)
+                | Wall -> 
+                    match map[y][1] with 
+                    | Field f -> (1, y, f)
+                    | Wall -> failwith "not possible"
+            | 'v' -> 
+                match map[y + 1][x] with 
+                | Field f -> (x, y + 1, f)
+                | Wall -> 
+                    match map[1][x] with 
+                    | Field f -> (x, 1, f)
+                    | Wall -> failwith "not possible"
+            | '<' -> 
+                match map[y][x - 1] with 
+                | Field f -> (x - 1, y, f)
+                | Wall -> 
+                    let width = map[y].Length
+                    match map[y][width - 2] with 
+                    | Field f -> (width - 2, y, f)
+                    | Wall -> failwith "not possible"
+            | '^' -> 
+                match map[x][y - 1] with 
+                | Field f -> (x, y - 1, f)
+                | Wall -> 
+                    let height = map.Length
+                    match map[height - 2][x] with 
+                    | Field f -> (x, height - 2, f)
+                    | Wall -> failwith "not possible"
+            | _ -> failwith "not possible"
+
+
+    let moveBlizzard blizzard remainingBlizzards (map: Cell[][]) y x = 
+        let (targetX, targetY, targetField) = getPositionToUpdate blizzard x y
+        map[y][x] <- Field remainingBlizzards
+        let newTargetField = Array.append targetField [| blizzard |] |> Field
+        map[targetY][targetX] <- newTargetField
+    
+    // TODO all fields should be updated simultaneously, not like now - we end-up moving the same blizzard multiple times
+    for i in 0 .. 1 .. 7 do 
+        // move blizzards
+        for y in 1 .. 1 .. map.Length - 2 do 
+            for x in 1 .. 1 .. map[0].Length - 2 do 
+                match map[y][x] with 
+                | Wall -> () 
+                | Field field -> 
+                    match List.ofArray field with 
+                    | [] -> () // empty field
+                    | blizzard::[] -> // field with one blizzard
+                        moveBlizzard blizzard [||] map y x
+                    | blizzard::blizzards -> // field with multiple blizzards
+                        moveBlizzard blizzard (Array.ofList blizzards) map y x
+        print map
+
 
