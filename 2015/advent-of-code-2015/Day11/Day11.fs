8fba9ea839b7273e27ece73a57550c1bc2b97778
UglyOne <omojzboze@protonmail.com>
2023-02-18 20:59:19 +0100

    fix broken project file

--- 2015/advent-of-code-2015/Day11/Day11.fs
+++ 2015/advent-of-code-2015/Day11/Day11.fs
@@ -3,5 +3,10 @@ module Day11
 open System.Collections.Generic
 open System.Text.RegularExpressions
 
+
+let increase text =
+   "hahah"
+   
 let run () =
+   "aa" |> increase |> printfn "%s"
    ()
