6f86694bdba5ceb9ac24face204fefc1656bffea
UglyOne <omojzboze@protonmail.com>
2023-02-21 20:49:19 +0100

    2015 day 14 done

--- 2015/advent-of-code-2015/Day14/Day14.fs
+++ 2015/advent-of-code-2015/Day14/Day14.fs
@@ -9,8 +9,9 @@ type Reindeer = {
     FlyTime: int
     RestTime: int
     mutable DistanceSoFar: int
-    mutable State: int // 1 flying. 0 resting
+    mutable State: bool // 1 flying. 0 resting
     mutable RemainingTimeForCurrentState: int
+    mutable Score: int
 }
 
 let parse line =
@@ -19,18 +20,35 @@ let parse line =
     let speed = match_.Groups[2] |> string |> int
     let flyTime = match_.Groups[3] |> string |> int
     let restTime = match_.Groups[4] |> string |> int
-    {Name = name; Speed = speed; FlyTime = flyTime; RestTime = restTime; DistanceSoFar = 0; State = 1; RemainingTimeForCurrentState = flyTime}
+    {Name = name; Speed = speed; FlyTime = flyTime; RestTime = restTime; DistanceSoFar = 0; State = true; RemainingTimeForCurrentState = flyTime; Score = 0}
     
 let run () =
     let input = [|
         "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
         "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
     |]
+    let input = InputReader.readLines "Day14/input.txt"
+    
     let reindeerCollection = input
                              |> Seq.map parse
                              |> Array.ofSeq
-    for second in 1 .. 1000 do
+    let findWinnersByDistance reindeerCollection =
+        let maxDistanceReindeer = reindeerCollection |> Seq.maxBy (fun r -> r.DistanceSoFar)
+        reindeerCollection |> Seq.filter (fun r -> r.DistanceSoFar = maxDistanceReindeer.DistanceSoFar)
+    
+    let findWinnerByScore reindeerCollection =
+        reindeerCollection |> Seq.maxBy (fun r -> r.Score)
+        
+    for second in 1 .. 2503 do
         for reindeer in reindeerCollection do
-            // TODO update reindeer 
-            
-    ()
+            if reindeer.State then reindeer.DistanceSoFar <- reindeer.DistanceSoFar + reindeer.Speed else () 
+            reindeer.RemainingTimeForCurrentState <- reindeer.RemainingTimeForCurrentState - 1
+            if reindeer.RemainingTimeForCurrentState = 0 then
+                reindeer.State <- not reindeer.State
+                reindeer.RemainingTimeForCurrentState <- if reindeer.State then reindeer.FlyTime else reindeer.RestTime
+            else ()                    
+        let winners = findWinnersByDistance reindeerCollection
+        for winner in winners do
+            winner.Score <- winner.Score + 1
+    
+    reindeerCollection |> findWinnerByScore |> (fun r -> printfn $"%s{r.Name} %d{r.Score}")             
