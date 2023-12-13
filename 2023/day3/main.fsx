4e8a89c58e54af4f7c012f27eb3725bfe1079031
UglyOne <omojzboze@protonmail.com>
2023-12-13 15:31:31 +0100

    day 3 part 1 done

--- 2023/day3/main.fsx
+++ 2023/day3/main.fsx
@@ -1,3 +1,92 @@
-﻿let readLines file = System.IO.File.ReadLines(file) |> Array.ofSeq
+﻿open System
 
-readLines "testInput.txt" |> printfn "%A"
+let readLines file = System.IO.File.ReadLines(file) |> Array.ofSeq
+
+let lines = readLines "input.txt"
+
+type Position = int * int
+type Length = int
+type Number = int * Position * Length
+type Symbol = Position
+
+let fold (symbols: Symbol[], numbers: Number[], currentNumber: string, index: int, row: int) character =
+    match character with
+    | '.' ->
+        if currentNumber.Length > 0 then
+            let numberAsInt = currentNumber |> int
+            let number = Number(numberAsInt, (index - currentNumber.Length, row), currentNumber.Length)
+            (symbols, Array.append numbers [| number |], "", index+1, row)
+        else
+            (symbols, numbers, currentNumber, index+1, row)
+    | digit when Char.IsDigit(digit) ->
+        let newCurrentNumber = currentNumber + (character |> string)
+        (symbols, numbers, newCurrentNumber, index+1, row)
+    | symbol ->
+        let newSymbol = Symbol(index, row)
+    
+        if currentNumber.Length > 0 then
+            let numberAsInt = currentNumber |> int
+            let number = Number(numberAsInt, (index - currentNumber.Length, row), currentNumber.Length)
+            let newNumbers = Array.append numbers [| number |]
+            (Array.append symbols [| newSymbol |], newNumbers, "", index+1, row)
+        else
+           (Array.append symbols [| newSymbol |], numbers, currentNumber, index+1, row)
+        
+
+let parseLine (line: string) (row: int) =
+    
+    let emptyNumbers : Number[] = [||]
+    let emptySymbols : Symbol[] = [||]
+    let (symbols, numbers, lastNumber, _, _) = line |> Seq.fold fold (emptySymbols, emptyNumbers, "", 0, row)
+    
+    if lastNumber.Length > 0
+    then
+        let valueAsInt = lastNumber |> int
+        let number = Number(valueAsInt, (line.Length - lastNumber.Length, row), lastNumber.Length)
+        ((Array.append numbers [| number |]), symbols)
+    else
+        (numbers, symbols)
+   
+let numbersPositions = lines |> Array.indexed |> Array.map (fun (index, line) -> parseLine line index)
+
+let (allNumbers : Number[], allSymbols : Symbol[]) =
+    numbersPositions
+    |> (Array.fold (fun (allNumbers, allSymbols) (numbers, symbols) -> (Array.append allNumbers numbers), (Array.append allSymbols symbols)) ([||], [||]))
+    
+printfn ""
+printfn ""
+
+let bla2 (position: Position) (symbols: Position) =
+    position = symbols
+
+let bla (position: Position) (symbols: Symbol[]) =
+    symbols |> Array.fold (fun state symbol -> (bla2 position symbol) || state) false
+    
+let getPositionsToCheck (number: Number) =
+    let (value, startPos, length) = number
+    let y = snd startPos
+    let x = fst startPos
+    let leftAndRight = [|
+        (x - 1, y)
+        (x + length, y)
+    |]
+    
+    let positionsWithinLine = seq { for i in (x-1)..(x + length) -> i }
+    
+    let upAndDown =
+        positionsWithinLine
+        |> Seq.map (fun i -> [| (i, y-1); (i, y + 1 )  |] )
+        |> Seq.fold (fun state item -> Array.append state item ) [|  |]
+        
+    Array.append leftAndRight upAndDown
+    
+
+let isNearSymbols (symbols: Symbol[]) (number: Number) =
+    let positionsToCheck : Position[] = getPositionsToCheck number
+    positionsToCheck |> Array.fold (fun state position -> (bla position symbols) || state) false
+
+allNumbers
+|> Array.filter (isNearSymbols allSymbols)
+|> Array.sumBy(fun (value, _, _) -> value)
+|> printfn "%A"
+
