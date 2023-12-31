df208968a13a0e881aaaa518658bd1ad88d470ba
UglyOne <omojzboze@protonmail.com>
2023-12-31 10:15:22 +0100

    day 9 part 1 done

---
+++ 2023/day9/main.fsx
@@ -0,0 +1,44 @@
+﻿#load "../general.fsx"
+
+open System.Collections.Generic
+
+let test1 = "0 3 6 9 12 15"
+let test2 = "1 3 6 10 15 21"
+let test3 = "10 13 16 21 30 45"
+
+let testInput = [| test1; test2; test3 |]
+
+let toNumbers (line: string) =
+    line.Split(' ') |> Array.map (fun n -> n |> int)
+
+let calculateDiffs (numbers: int array) =
+    let result: List<int> = List<int>()
+    let differences =
+        numbers[1..] |>
+        (Array.fold (fun previous current ->
+                        result.Add(current - previous)
+                        current)
+                    (numbers[0]))
+    result
+ 
+let areAllZeros (numbers: int array) =
+    numbers |> Array.fold (fun state number -> state && number = 0) true
+    
+let rec calculateDiffsRec (numbers: int array) =
+    if areAllZeros numbers then numbers[(numbers.Length-1)]
+    else
+        let diffs = calculateDiffs numbers |> Array.ofSeq
+        // diffs |> General.print
+        let lastNumberFromDiffs = calculateDiffsRec diffs
+        let lastNumber = numbers[(numbers.Length-1)]
+        let newLastNumber = lastNumber + lastNumberFromDiffs
+        newLastNumber   
+  
+let getNextValue (line) =
+    line |>toNumbers |> calculateDiffsRec
+    
+let getSum input =
+    input |> Array.map (toNumbers) |> Array.map calculateDiffsRec |> Array.sum |> General.print
+    
+testInput |> getSum
+General.readLines "input.txt" |> getSum
