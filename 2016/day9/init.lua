1d59b16daa592b7125b1e83eef0474ee1d3ac3cd
UglyOne <omojzboze@protonmail.com>
2025-02-15 22:47:09 +0100

    2016 day9 part2

--- 2016/day9/init.lua
+++ 2016/day9/init.lua
@@ -3,51 +3,55 @@ local lines = lib.getLines()
 
 local function decompress(line)
   local line_array = lib.to_table(line)
-  local result = ""
   local reading_instruction = false
   local reading_second = false
-  local skip_count = 0
   local characters_count_string = ""
   local times_string = ""
+  local counts = {}
   for i = 1, #line_array do
-    if skip_count > 0 then
-      skip_count = skip_count - 1
-    else
-      local char = line_array[i]
-      if char == '(' then
-        reading_instruction = true
-      elseif char == ')' and reading_instruction then
-        local characters_count = tonumber(characters_count_string)
-        local times = tonumber(times_string)
-        local characters = ""
-        for c = 1, characters_count do
-          characters = characters .. line_array[i + c]
-        end
-        for t = 1, times do
-          result = result .. characters
-        end
-        reading_instruction = false
-        reading_second = false
-        characters_count_string = ""
-        times_string = ""
-        skip_count = characters_count
-      elseif reading_instruction == true then
-        if char == 'x' then
-          reading_second = true
+    local char = line_array[i]
+    if char == '(' then
+      counts[i] = 0
+      reading_instruction = true
+    elseif char == ')' and reading_instruction then
+      counts[i] = 0
+      local characters_count = tonumber(characters_count_string)
+      local times = tonumber(times_string)
+      for c = 1, characters_count do
+        if counts[i + c] == nil then
+          counts[i + c] = times
         else
-          if reading_second then
-            times_string = times_string .. char
-          else
-            characters_count_string = characters_count_string ..
-                char
-          end
+          counts[i + c] = counts[i + c] * times
         end
+      end
+      reading_instruction = false
+      reading_second = false
+      characters_count_string = ""
+      times_string = ""
+    elseif reading_instruction == true then
+      counts[i] = 0
+      if char == 'x' then
+        reading_second = true
       else
-        result = result .. char
+        if reading_second then
+          times_string = times_string .. char
+        else
+          characters_count_string = characters_count_string ..
+              char
+        end
+      end
+    else
+      if counts[i] == nil then
+        counts[i] = 1
       end
     end
   end
-  return #result
+
+  local sum = 0
+  for k, v in pairs(counts) do
+    sum = sum + v
+  end
+  return sum
 end
 
 for i = 1, #lines do
