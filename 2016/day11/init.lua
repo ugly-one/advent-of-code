ae177050cd7f3c95e0d27b13f386b9954e9968e7
UglyOne <omojzboze@protonmail.com>
2025-02-19 12:57:36 +0100

    2016 day11 part1

--- 2016/day11/init.lua
+++ 2016/day11/init.lua
@@ -1,191 +1,194 @@
 local lib = require('lib')
-local lines = lib.getLines('test.txt')
-local floors = {}
+local lines = lib.getLines()
+local floors = { {}, {}, {}, {} }
 local types = {}
+local types_count = 0
+local chip_indexes = {}
+local generator_indexes = {}
+local index = 1
 for i, line in ipairs(lines) do
-  local floor = {}
   local chips_string = string.gmatch(line, "a (%a+)-compatible microchip")
   for c in chips_string do
-    table.insert(types, c)
-    table.insert(floor, { name = c, chip = true })
+    types[c] = index
+    floors[i][index] = true
+    table.insert(chip_indexes, index)
+    index = index + 2
+    types_count = types_count + 1
   end
+end
 
+for i = 1, 4 do
+  for j = 1, types_count * 2 do
+    if not floors[i][j] then floors[i][j] = false end
+  end
+end
+
+for i, line in ipairs(lines) do
   local generators_string = string.gmatch(line, "a (%a+) generator")
   for g in generators_string do
-    table.insert(floor, { name = g, generator = true })
+    local index = types[g] + 1
+    table.insert(generator_indexes, index)
+    floors[i][index] = true
   end
-  table.insert(floors, floor)
 end
 
-local types_to_id = {}
-for i, type in ipairs(types) do
-  types_to_id[type] = i
+local floor_size = types_count * 2
+
+local function to_string(floor)
+  local line = ""
+  for j = 1, floor_size do
+    if floor[j] then line = line .. 'x' else line = line .. ' ' end
+  end
+  return line
 end
-local function generate_all_moves(options, current, result)
-  if #current == 2 then return end
+local function to_string_map(floors)
+  local result = ""
+  for i = 4, 1, -1 do
+    local line = to_string(floors[i])
+    result = result .. line
+  end
+  return result
+end
+
+
+local function get_indexes(floor)
+  local indexes = {}
+  for i = 1, floor_size do
+    if floor[i] then table.insert(indexes, i) end
+  end
+  return indexes
+end
+
+local function get_permutations_rec(options, current, result)
+  if #current == 2 then
+    if math.abs(current[1] - current[2]) == 1 then
+      for _, r in ipairs(result) do
+        if #r == 2 then
+          if math.abs(r[1] - r[2]) == 1 then return end
+        end
+      end
+    end
+    table.insert(result, current)
+    return
+  end
+  if #current == 1 then
+    table.insert(result, current)
+  end
   for i = 1, #options do
+    local current_copy = lib.copy_table(current)
+    table.insert(current_copy, options[i])
     local options_copy = {}
     for j = i + 1, #options do
       table.insert(options_copy, options[j])
     end
-    local current_copy = lib.copy_table(current)
-    table.insert(current_copy, options[i])
-    table.insert(result, current_copy)
-    generate_all_moves(options_copy, current_copy, result)
+    get_permutations_rec(options_copy, current_copy, result)
   end
 end
 
-
-local function remove_invalid_pairs(options)
-  for i, option in ipairs(options) do
-    if #option == 2 then
-      if (option[1].chip and option[2].generator) or (option[2].chip and option[1].generator)
-          and not (option[1].name == option[2].name) then
-        table.remove(options, i)
-      end
-    end
-  end
-end
-
-local function generate(options)
+local function get_permutations(options)
   local result = {}
-  local current = {}
-  generate_all_moves(options, current, result)
-  remove_invalid_pairs(result)
+  get_permutations_rec(options, {}, result)
   return result
 end
 
-local function is_valid_floor(floor)
-  local map = {}
-  for i = 1, #floor do
-    local item = floor[i]
-    if map[item.name] then
-      map[item.name] = 'safe'
-    else
-      if item.generator then
-        map[item.name] = 'dangerous'
-      else
-        map[item.name] = 'chip'
-      end
-    end
-  end
-
-  local has_unprotected_chip = false
-  local has_dangerous_generator = false
-  for k, v in pairs(map) do
-    if v == 'dangerous' then has_dangerous_generator = true end
-    if v == 'chip' then has_unprotected_chip = true end
-  end
-  if has_unprotected_chip and has_dangerous_generator then return false else return true end
-end
 
 local function copy_floors(floors)
-  local copy = {}
+  local result = {}
   for i = 1, 4 do
-    table.insert(copy, lib.copy_table(floors[i]))
+    table.insert(result, lib.copy_table(floors[i]))
   end
-  return copy
+  return result
 end
 
-local function simulate_option(floors, option, source_floor_id, target_floor_id)
-  local copy_floors = copy_floors(floors)
-  local source_floor = copy_floors[source_floor_id]
-  local target_floor = copy_floors[target_floor_id]
-  for _, item in ipairs(option) do
-    local item_id_to_remove = 0
-    for i, floor_item in ipairs(source_floor) do
-      if floor_item.name == item.name and floor_item.chip == item.chip and floor_item.generator == item.generator then
-        item_id_to_remove = i
+local visited = {}
+
+local function entire_floor_occupied(floor)
+  for i = 1, #floor do
+    if not floor[i] then return false end
+  end
+  return true
+end
+
+local function is_valid(floor)
+  local non_paired_chips = false
+  for i = 1, floor_size, 2 do
+    if floor[i] then
+      if floor[i + 1] == false then
+        non_paired_chips = true
+        break
       end
     end
-    table.remove(source_floor, item_id_to_remove)
-
-    table.insert(target_floor, item)
   end
-
-  if is_valid_floor(source_floor) and is_valid_floor(target_floor) then
-    return copy_floors
-  else
-    return nil
+  local non_paired_generators = false
+  for i = 1, floor_size, 2 do
+    if floor[i + 1] then
+      if floor[i] == false then
+        non_paired_generators = true
+        break
+      end
+    end
   end
+  if non_paired_chips and non_paired_generators then return false else return true end
 end
 
-local function print_floors(floors)
-  for i = 4, 1, -1 do
-    local floor = floors[i]
+local min = 100
+local function part1(floors, steps, floor, steps_taken, previous_direction)
+  if steps > min then return end
+  if entire_floor_occupied(floors[4]) then
+    if steps < min then min = steps end
     local line = ""
-    for _, item in ipairs(floor) do
-      local type = 'M'
-      if item.chip then type = 'M' else type = 'G' end
-      line = line .. ' ' .. string.upper(string.sub(item.name, 1, 1)) .. type
+    for _, step in ipairs(steps_taken) do
+      line = line .. ' -> ' .. step
     end
-    print(line)
-  end
-end
-
-local min = 30
-
-local function equal_option(o1, o2)
-  if #o1 == #o2 then
-    for i = 1, #o1 do
-      local match = false
-      for j = 1, #o2 do
-        if o1[i].name == o2[j].name and o1[i].chip == o2[j].chip then match = true end
-      end
-      if not match then return false end
-    end
-    return true
-  end
-  return false
-end
-
-local checked_maps = {}
-
-local function try_move_all_to_last_floor(
-    floors,
-    current_floor_id,
-    current_step,
-    previous_move,
-    previous_moves)
-  if current_step > min then return end
-
-  if #floors[1] == 0 and #floors[2] == 0 and #floors[3] == 0 and current_step < min then
-    print(' new best !', current_step)
-    min = current_step
+    print("new min", steps, line)
     return
   end
-  local floor = floors[current_floor_id]
-  local all_options = generate(floor)
+  local options = get_permutations(get_indexes(floors[floor]))
   local directions = {}
-  if current_floor_id < 4 and current_floor_id > 1 then
-    directions = { 1, -1 }
-  elseif current_floor_id == 4 then
-    directions = { -1 }
-  else
+  if floor == 1 then
     directions = { 1 }
+  elseif floor == 4 then
+    directions = { -1 }
+  elseif floor == 2 or floor == 3 then
+    if previous_direction == -1 then
+      directions = { 1 }
+    else
+      directions = { 1, -1 }
+    end
   end
-
-  for _, option in ipairs(all_options) do
+  for _, option in ipairs(options) do
     for _, direction in ipairs(directions) do
-      local new_move = { option = option, direction = direction }
-      if previous_move and new_move.direction == -previous_move.direction and equal_option(new_move.option, previous_move.option) then
+      if #option == 1 and direction == 1 then
+        -- do not try to bring one item up. going with one item is slow - let's try to make this assumption
+      elseif #option == 2 and direction == -1 then
+        -- do not bring 2 things down. I think it makes sense to only bring one down.
       else
-        local new_floor = simulate_option(floors, option, current_floor_id, current_floor_id + direction)
-        if new_floor then
-          --local new_previous_moves = {}
-          --for i = 1, #previous_moves do
-          --  table.insert(new_previous_moves,
-          --    { option = lib.copy_table(previous_moves[i].option), direction = previous_moves[i].direction })
-          --end
-          --table.insert(new_previous_moves, new_move)
-          try_move_all_to_last_floor(new_floor, current_floor_id + direction, current_step + 1, new_move,
-            previous_moves)
+        local floors_copy = copy_floors(floors)
+        local step_taken = tostring(direction)
+        for _, item in ipairs(option) do
+          floors_copy[floor][item] = false
+          floors_copy[floor + direction][item] = true
+          step_taken = step_taken .. ' ' .. item
+        end
+        if is_valid(floors_copy[floor]) and is_valid(floors_copy[floor + direction]) then
+          local hash = to_string_map(floors_copy) .. tostring(floor + direction)
+          local maybe_visited = visited[hash]
+          local steps_taked_copy = lib.copy_table(steps_taken)
+          table.insert(steps_taked_copy, step_taken)
+          if not maybe_visited then
+            visited[hash] = steps + 1
+            part1(floors_copy, steps + 1, floor + direction, steps_taked_copy, direction)
+          else
+            if steps + 1 < maybe_visited then
+              visited[hash] = steps + 1
+              part1(floors_copy, steps + 1, floor + direction, steps_taked_copy, direction)
+            end
+          end
         end
       end
     end
   end
 end
 
-try_move_all_to_last_floor(floors, 1, 0, nil, {})
-
+part1(floors, 0, 1, {}, -2)
 print(min)
