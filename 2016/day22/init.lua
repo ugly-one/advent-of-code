3f1441c1f548e26deb534e4cb1c124ff1b5a0f8f
UglyOne <omojzboze@protonmail.com>
2025-02-25 20:49:48 +0100

    2016 day22 part1 a bit faster

--- 2016/day22/init.lua
+++ 2016/day22/init.lua
@@ -10,33 +10,35 @@ local function parse(line)
   local spaces = string.gmatch(line, "(%d+)T")
   return { x = x, y = y, size = tonumber(spaces()), used = tonumber(spaces()), avail = tonumber(spaces()) }
 end
-
+local flat_nodes = {}
 local nodes = {}
 for i = 1, #input do
   if string.sub(input[i], 1, 1) == '/' then
-    table.insert(nodes, parse(input[i]))
+    local node = parse(input[i])
+    if nodes[node.y] then
+      nodes[node.y][node.x] = node
+    else
+      nodes[node.y] = {}
+      nodes[node.y][node.x] = node
+    end
+    table.insert(flat_nodes, node)
   end
 end
 
 local function count_pairs(nodes)
   local pairs_count = 0
   local pairs = {}
-  for i, node in ipairs(nodes) do
-    local to_compare = lib.copy_table(nodes)
-    table.remove(to_compare, i)
-    for j, another_node in ipairs(to_compare) do
-      if node.used > 0 and another_node.avail >= node.used then
-        local hash = node.x .. '.' .. node.y .. '.' .. another_node.x .. '.' .. another_node.y
-        local another_hash = another_node.x .. '.' .. another_node.y .. '.' .. node.x .. '.' .. node.y
-        if pairs[hash] or pairs[another_hash] then
-        else
-          pairs[hash] = true
-          pairs_count = pairs_count + 1
-        end
+  for i = 1, #nodes - 1 do
+    for j = i + 1, #nodes do
+      local node = nodes[i]
+      local another_node = nodes[j]
+      if (node.used > 0 and another_node.avail >= node.used) or (another_node.used > 0 and node.avail >= another_node.used) then
+        pairs_count = pairs_count + 1
       end
     end
   end
+
   print(pairs_count)
 end
 
-count_pairs(nodes)
+count_pairs(flat_nodes)
