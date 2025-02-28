70c6e7f0449ade560b0300c856963ea7f153ca47
UglyOne <omojzboze@protonmail.com>
2025-02-28 09:04:49 +0100

    2016 day22 part2

--- 2016/day22/init.lua
+++ 2016/day22/init.lua
@@ -27,7 +27,6 @@ end
 
 local function count_pairs(nodes)
   local pairs_count = 0
-  local pairs = {}
   for i = 1, #nodes - 1 do
     for j = i + 1, #nodes do
       local node = nodes[i]
@@ -42,3 +41,43 @@ local function count_pairs(nodes)
 end
 
 count_pairs(flat_nodes)
+
+local rows = #nodes
+local columns = #nodes[1]
+
+local char_nodes = {}
+for i = 1, #input do
+  if string.sub(input[i], 1, 1) == '/' then
+    local node = parse(input[i])
+    if not char_nodes[node.y] then
+      char_nodes[node.y] = {}
+    end
+    local char = '.'
+    if node.used == 0 then
+      char = '_'
+    elseif node.used > 300 then
+      char = '#'
+    end
+    char_nodes[node.y][node.x] = char
+  end
+end
+
+char_nodes[1][columns] = 'G'
+
+local function print_nodes(nodes)
+  for y = 1, rows do
+    local line = ""
+    for x = 1, columns do
+      line = line .. nodes[y][x]
+    end
+    print(line)
+  end
+end
+
+print_nodes(char_nodes)
+
+local distance_to_door = 17 - 6 + 24 - 13
+local distance_from_door_to_first_front = 37 - 6 + 13 - 1
+local distance = distance_from_door_to_first_front + distance_to_door
+
+print(distance + 36 * 5 + 1)
