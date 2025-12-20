ebbc8cddc73ca0766199c594aec1611db568b9af
UglyOne <omojzboze@protonmail.com>
2025-12-20 21:22:10 +0100

    2025 day3 part2

--- 2025/day3/init.lua
+++ 2025/day3/init.lua
@@ -4,7 +4,7 @@ local input = lib.getLines()
 
 local result = 0
 
-for _, line in ipairs(input) do
+local get_largest_joltage = function(line)
   local line_table = lib.to_table(line)
   local first_digit = 0
   local first_digit_index = 0
@@ -25,7 +25,40 @@ for _, line in ipairs(input) do
   end
 
   local largest_joltage = tonumber(first_digit .. second_digit)
-  result =result + largest_joltage
+  return largest_joltage
+end
+
+local get_largest_joltage_part2 = function(line, batteries_count)
+  local line_table = lib.to_table(line)
+
+  local digits = ''
+  local last_digit_index = 0
+
+  for battery_nr = 1, batteries_count, 1 do
+    -- print('battery nr:', battery_nr)
+    local digit = 0
+    for index = last_digit_index + 1, #line_table - batteries_count + battery_nr, 1 do
+      -- print('index:', index)
+      local number = tonumber(line_table[index])
+      if (number > digit) then
+        last_digit_index = index
+        digit = number
+      end
+      -- print(last_digit_index, digit, digits)
+    end
+
+    digits = digits .. digit
+  end
+
+  return tonumber(digits)
+end
+
+-- print(get_largest_joltage_part2('12345', 3))
+
+for _, line in ipairs(input) do
+  -- local largest_joltage = get_largest_joltage(line)
+  local largest_joltage = get_largest_joltage_part2(line, 12)
+  result = result + largest_joltage
 end
 
 print('result:', result)
