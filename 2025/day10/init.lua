acabbc090d949384857a7881190475a3bbf786c6
UglyOne <omojzboze@protonmail.com>
2026-01-19 17:13:30 +0100

    2025 day10 part1 - faster

--- 2025/day10/init.lua
+++ 2025/day10/init.lua
@@ -2,45 +2,6 @@ local lib = require('lib')
 local input = lib.getLines()
 -- input = lib.getLines('test.txt')
 
-local function bla(current_lights, remaining_buttons, current_step, target_lights, minimum)
-  if current_lights == target_lights then
-    if minimum == nil or current_step < minimum then
-      return current_step
-    end
-  end
-
-  if #remaining_buttons == 0 then return minimum end
-
-  if minimum ~= nil and current_step > minimum then return minimum end
-
-  local new_min = minimum
-
-  for index, button in ipairs(remaining_buttons) do
-    if not button.used then
-      remaining_buttons[index].used = true
-
-      local new_lights = bit.bxor(current_lights, button.value)
-
-      local new_min_option = bla(new_lights, remaining_buttons, current_step + 1, target_lights, new_min)
-
-      remaining_buttons[index].used = false
-
-      if new_min_option ~= nil and (new_min == nil or new_min_option < new_min) then
-        new_min = new_min_option
-      end
-    end
-  end
-
-  return new_min
-end
-
-local function find_minimal_presses(indicator_lights_goal, buttons)
-  local result = bla(0, buttons, 0, indicator_lights_goal, nil)
-
-  return result
-end
-
-
 local result = 0
 
 local lines = {}
@@ -56,23 +17,61 @@ for _, line in ipairs(input) do
 
   local buttons = {}
   for button_string in string.gmatch(line, "%((.-)%)") do
-    local button_number = 0
-    for light in string.gmatch(button_string, "%d+") do
-      local light_number = tonumber(light)
-      assert(light_number)
-      button_number = button_number + math.pow(2, light_number)
+    local light_number = 0
+    for light_index_string in string.gmatch(button_string, "%d+") do
+      local light_index = tonumber(light_index_string)
+      assert(light_index)
+      light_number = light_number + math.pow(2, light_index)
     end
-    table.insert(buttons, { value = button_number, used = false })
+    table.insert(buttons, light_number)
   end
   local joltage_requirements = string.match(line, "%{(.-)%}")
 
   table.insert(lines, { target = lights_as_number, buttons = buttons })
 end
 
-for index, line in ipairs(lines) do
-  local minPresses = find_minimal_presses(line.target, line.buttons)
-  result = result + minPresses
+local function generate_combinations(available, combination, all)
+  if #available == 0 then return end
+
+  local n = #available
+  for i = 1, n do
+    local value = available[i]
+    local remaining = { unpack(available, i + 1, n) }
+    local new_combination = lib.copy_table(combination)
+    table.insert(new_combination, value)
+    table.insert(all, new_combination)
+    generate_combinations(remaining, new_combination, all)
+  end
+end
+
+local function press(combination)
+  local light = 0
+  for i = 1, #combination do
+    local button = combination[i]
+    light = bit.bxor(light, button)
+  end
+  return light
+end
+
+for _, line in ipairs(lines) do
+  local combinations = {}
+  generate_combinations(line.buttons, {}, combinations)
+
+  local steps = nil
+
+  for i = 1, #combinations do
+    local combination = combinations[i]
+    local light = press(combination)
+    if light == line.target then
+      if steps == nil or #combination < steps then
+        steps = #combination
+      end
+    end
+  end
+
+  result = result + steps
 end
 
 print('result:', result)
 vim.fn.setreg('+', result)
+
